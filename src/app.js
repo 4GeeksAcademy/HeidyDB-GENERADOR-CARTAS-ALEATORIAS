5f87c889a2737869f7ad193616f76e381ed141f0
HeidyDB <heidydiazbarrios@gmail.com>
2025-04-17 21:28:03 +0000

    segundo

--- src/app.js
+++ src/app.js
@@ -6,9 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  
- 
+ generateRandomCard()} 
 
+  function generateRandomCard(){
+     let nrosCarta = [ '2', '3', '4','5', '6', '7', '8', '9', '10', 'Q', 'K', 'J', 'A'];
+     let palosCarta = ['heart ', 'diamond', 'spade', 'club'];
 
+      let crearRandomNros = Math.floor(Math.random()*nrosCarta.length); 
+      let crearRandomPalo = Math.floor(Math.random()*palosCarta.length); 
+
+      const valorCarta = nrosCarta[crearRandomNros]; //posicion del arreglo numeros
+      const paloCarta = palosCarta[crearRandomPalo]; // posicion del arreglo de palos   
+      
+
+   //Seleccionar los elementos del Dom
+   const cardElement = document.getElementById("card")
+   const cardSymbol = document.getElementById("card-symbol")
+   //const topValue = document.getElementById("top-value")
+   //const bottomValue = document.getElementById("bottom-value")
+   cardElement.classList.add(paloCarta)
+   cardSymbol.innerHTML = valorCarta
+   document.getElementById('card').classList.add(claseCarta(paloCarta));
+    
 };
+
+
+function claseCarta(paloCarta){
+  if (paloCarta == "heart") return "hearts";
+  if (paloCarta == "diamond") return "diamonds";
+  if (paloCarta == "spade") return "spades";
+  if (paloCarta == "club") return "clubs";
+} 
